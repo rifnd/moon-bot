e3b7dee25b0c1fe80d8a6a63fbe3c76397fb7ead
nando <Rifando.p.p@gmail.com>
2023-08-06 08:02:25 +0700

    update plugins/downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -5,6 +5,7 @@ let handler = async (m, {
 }) => {
   if (!args[0]) return m.reply(Func.example(usedPrefix, command, 'https://youtube.com/watch?v=-BaHui7--ak'))
   if (!/^(?:https?:\/\/)?(?:www\.|m\.|music\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/.test(args[0])) return m.reply(status.invalid)
+  m.reply(status.wait)
   try {
     const json = await Func.fetchJson(API('alya', '/api/ytv', { url: args[0] }, 'apikey'))
     if (!json.status) return m.reply(Func.jsonFormat(json))
