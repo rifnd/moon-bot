dec6ae8de72efe30c9cb9d2303242c1af828bfb8
nando <Rifando.p.p@gmail.com>
2023-08-06 08:43:20 +0700

    update plugins/downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -11,18 +11,24 @@ let handler = async (m, {
         if (!json.status) return m.reply(Func.jsonFormat(json))
         let result = json.data.find(v => v.quality == 'HD' && v.response == 200)
         if (result) {
-            conn.sendFile(m.chat, result.url, '', 'Kualitas HD', m)
+            conn.sendMedia(m.chat, result.url, m, {
+              caption: 'Kualitas HD',
+              mentions: [m.sender]
+            })
         } else {
             let result = json.data.find(v => v.quality == 'SD' && v.response == 200)
             if (!result) return m.reply(status.fail)
-            conn.sendFile(m.chat, result.url, '', 'Kualitas SD', m)
+            conn.sendMedia(m.chat, result.url, m, {
+              caption: 'Kualitas SD',
+              mentions: [m.sender]
+            })
         }
     } catch (e) {
         console.log(e)
         return m.reply(status.error)
     }
 }
-handler.help = ['facebook'].map(v => v + ' <url>')
+handler.help = ['facebook']
 handler.tags = ['downloader']
 handler.command = /^(fb|facebook)$/i
 handler.limit = 1
