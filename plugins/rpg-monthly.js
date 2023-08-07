aa753ed8a97a696870a4c77f7283b8eee549d689
nando <Rifando.p.p@gmail.com>
2023-08-07 12:26:22 +0700

    update plugins/rpg-monthly.js

--- plugins/rpg-monthly.js
+++ plugins/rpg-monthly.js
@@ -5,16 +5,18 @@ const limitprem = 40
 const moneyfree = 20000
 const moneyprem = 40000
 
-let handler = async (m, { isPrems, conn }) => {
+let handler = async (m, {
+  isPrems, conn
+}) => {
   if (!db.data.settings[conn.user.jid].rpg) throw dfail('rpg', m, conn)
   let time = global.db.data.users[m.sender].lastmonthly + 2592000000
   if (new Date - global.db.data.users[m.sender].lastmonthly < 2592000000) throw `Anda sudah mengklaim, klaim bulanan ini\ntunggu selama ${msToTime(time - new Date())} lagi`
   // conn.reply(m.chat, `Anda sudah mengklaim dan mendapatkan :`, m)
-  global.db.data.users[m.sender].exp += isPrems ? prem : free
-  global.db.data.users[m.sender].money += isPrems ? moneyprem : moneyfree
-  global.db.data.users[m.sender].limit += isPrems ? limitprem : limitfree
+  global.db.data.users[m.sender].exp += isPrems ? prem: free
+  global.db.data.users[m.sender].money += isPrems ? moneyprem: moneyfree
+  global.db.data.users[m.sender].limit += isPrems ? limitprem: limitfree
   // global.db.data.users[m.sender].pet += 3
-  conn.reply(m.chat, `Selamat kamu mendapatkan:\n\n+${isPrems ? prem : free} Exp\n+${isPrems ? moneyprem : moneyfree} Money\n+${isPrems ? limitprem : limitfree} Limit`, m)
+  conn.reply(m.chat, `Selamat kamu mendapatkan:\n\n+${isPrems ? prem: free} Exp\n+${isPrems ? moneyprem: moneyfree} Money\n+${isPrems ? limitprem: limitfree} Limit`, m)
   global.db.data.users[m.sender].lastmonthly = new Date * 1
 }
 handler.help = ['monthly']
@@ -28,15 +30,15 @@ module.exports = handler
 
 function msToTime(duration) {
   var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24),
-    monthly = Math.floor((duration / (1000 * 60 * 60 * 24)) % 720)
+  seconds = Math.floor((duration / 1000) % 60),
+  minutes = Math.floor((duration / (1000 * 60)) % 60),
+  hours = Math.floor((duration / (1000 * 60 * 60)) % 24),
+  monthly = Math.floor((duration / (1000 * 60 * 60 * 24)) % 720)
 
-  monthly = (monthly < 10) ? "0" + monthly : monthly
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
+  monthly = (monthly < 10) ? "0" + monthly: monthly
+  hours = (hours < 10) ? "0" + hours: hours
+  minutes = (minutes < 10) ? "0" + minutes: minutes
+  seconds = (seconds < 10) ? "0" + seconds: seconds
 
   return monthly + " hari " + hours + " jam " + minutes + " menit"
 }
