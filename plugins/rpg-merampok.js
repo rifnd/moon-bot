d7f94acf3e00e1057508f7da99962e66975a4efc
nando <Rifando.p.p@gmail.com>
2023-08-07 12:25:27 +0700

    update plugins/rpg-merampok.js

--- plugins/rpg-merampok.js
+++ plugins/rpg-merampok.js
@@ -1,6 +1,8 @@
 // let pajak = 0.02
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!db.data.settings[conn.user.jid].rpg) throw dfail('rpg', m, conn)
+let handler = async (m, {
+  conn, text, usedPrefix, command
+}) => {
+  if (!db.data.settings[conn.user.jid].rpg) return m.reply(status.rpg)
   let dapat = (Math.floor(Math.random() * 10000))
   let nomors = m.sender
   let who
@@ -38,6 +40,10 @@ function clockString(ms) {
   let h = Math.floor(ms / 3600000)
   let m = Math.floor(ms / 60000) % 60
   let s = Math.floor(ms / 1000) % 60
-  console.log({ ms, h, m, s })
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+  console.log({
+    ms, h, m, s
+  })
+  return [h,
+    m,
+    s].map(v => v.toString().padStart(2, 0)).join(':')
 }
