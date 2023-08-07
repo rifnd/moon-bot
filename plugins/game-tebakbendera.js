59cb85675ccca78c1d7f7665cf63b63a7d1d147d
nando <Rifando.p.p@gmail.com>
2023-08-07 12:42:46 +0700

    update plugins/game-tebakbendera.js

--- plugins/game-tebakbendera.js
+++ plugins/game-tebakbendera.js
@@ -41,31 +41,35 @@ let timeout = 120000
 let poin = 1000
 let tiketcoin = 1
 let src
-let handler = async (m, { conn, usedPrefix }) => {
-    if (!db.data.settings[conn.user.jid].game) throw dfail('game', m, conn)
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-    let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
-        throw false
-    }
-    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/qisyana/scrape/main/flag.json'))).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    if (!json) throw json
-    let caption = `
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}tekbe untuk bantuan
-Bonus: ${poin} XP
-Tiketcoin: ${tiketcoin} TiketCoin
-`.trim()
-    conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.img, 'img.jpg', caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakbendera[id][0])
-            delete conn.tebakbendera[id]
-        }, timeout)
-    ]
+let handler = async (m, {
+  conn, usedPrefix
+}) => {
+  if (!db.data.settings[conn.user.jid].game) return m.reply(status.game)
+  conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera: {}
+  let id = m.chat
+  if (id in conn.tebakbendera) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
+    throw false
+  }
+  if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/qisyana/scrape/main/flag.json'))).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  if (!json) throw json
+  let caption = `
+  Timeout *${(timeout / 1000).toFixed(2)} detik*
+  Ketik ${usedPrefix}tekbe untuk bantuan
+  Bonus: ${poin} XP
+  Tiketcoin: ${tiketcoin} TiketCoin
+  `.trim()
+  conn.tebakbendera[id] = [
+    await conn.sendFile(m.chat, json.img, 'img.jpg', caption, m),
+    json,
+    poin,
+    setTimeout(() => {
+      if (conn.tebakbendera[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakbendera[id][0])
+      delete conn.tebakbendera[id]
+    },
+      timeout)
+  ]
 }
 handler.help = ['tebakbendera']
 handler.tags = ['game']
