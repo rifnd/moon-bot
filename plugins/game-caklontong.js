37a32bdccb3fc223736d57f56322949c82d4189c
nando <Rifando.p.p@gmail.com>
2023-08-07 12:33:58 +0700

    update plugins/game-caklontong.js

--- plugins/game-caklontong.js
+++ plugins/game-caklontong.js
@@ -3,32 +3,36 @@ let fetch = require('node-fetch')
 let timeout = 180000
 let poin = 500
 let tiketcoin = 1
-let handler = async (m, { conn, usedPrefix }) => {
-    if (!db.data.settings[conn.user.jid].game) throw dfail('game', m, conn)
-    conn.caklontong = conn.caklontong ? conn.caklontong : {}
-    let id = m.chat
-    if (id in conn.caklontong) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
-        throw false
-    }
-    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `
-${json.soal}
+let handler = async (m, {
+  conn, usedPrefix
+}) => {
+  if (!db.data.settings[conn.user.jid].game) return m.reply(status.game)
+  conn.caklontong = conn.caklontong ? conn.caklontong: {}
+  let id = m.chat
+  if (id in conn.caklontong) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+    throw false
+  }
+  let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `
+  ${json.soal}
 
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}calo untuk bantuan
-Bonus: ${poin} XP
-Tiketcoin: ${tiketcoin} TiketCoin
-`.trim()
-    conn.caklontong[id] = [
-        await conn.reply(m.chat, caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, conn.caklontong[id][0])
-            delete conn.caklontong[id]
-        }, timeout)
-    ]
+  Timeout *${(timeout / 1000).toFixed(2)} detik*
+  Ketik ${usedPrefix}calo untuk bantuan
+  Bonus: ${poin} XP
+  Tiketcoin: ${tiketcoin} TiketCoin
+  `.trim()
+  conn.caklontong[id] = [
+    await conn.reply(m.chat, caption, m),
+    json,
+    poin,
+    setTimeout(() => {
+      if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, conn.caklontong[id][0])
+      delete conn.caklontong[id]
+    },
+      timeout)
+  ]
 }
 handler.help = ['caklontong']
 handler.tags = ['game']
