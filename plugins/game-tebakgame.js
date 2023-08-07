890c15cc8452919e9b1310573d8fd279725082fa
nando <Rifando.p.p@gmail.com>
2023-08-07 12:43:28 +0700

    update plugins/game-tebakgame.js

--- plugins/game-tebakgame.js
+++ plugins/game-tebakgame.js
@@ -3,30 +3,36 @@ let fetch = require('node-fetch')
 let timeout = 180000
 let poin = 1000
 let tiketcoin = 1
-let handler = async (m, { conn, usedPrefix }) => {
-  if (!db.data.settings[conn.user.jid].game) throw dfail('game', m, conn)
-  conn.tebakgame = conn.tebakgame ? conn.tebakgame : {}
+let handler = async (m, {
+  conn, usedPrefix
+}) => {
+  if (!db.data.settings[conn.user.jid].game) return m.reply(status.game)
+  conn.tebakgame = conn.tebakgame ? conn.tebakgame: {}
   let id = m.chat
   if (id in conn.tebakgame) {
     conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgame[id][0])
     throw false
   }
   let src = await (await fetch('https://raw.githubusercontent.com/qisyana/scrape/main/tebakgame.json')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
+  let json = src[Math.floor(Math.random() * src.length)]
   // if (!json.status) throw json
   let caption = `
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}tega untuk clue
-Bonus: ${poin} XP
-TiketCoin: ${tiketcoin}
-    `.trim()
+  Timeout *${(timeout / 1000).toFixed(2)} detik*
+  Ketik ${usedPrefix}tega untuk clue
+  Bonus: ${poin} XP
+  TiketCoin: ${tiketcoin}
+  `.trim()
   conn.tebakgame[id] = [
-    await conn.sendFile(m.chat, json.img, 'tebakgame.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
-    json, poin,
+    await conn.sendFile(m.chat, json.img, 'tebakgame.jpg', caption, m, false, {
+      thumbnail: Buffer.alloc(0)
+    }),
+    json,
+    poin,
     setTimeout(() => {
       if (conn.tebakgame[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakgame[id][0])
       delete conn.tebakgame[id]
-    }, timeout)
+    },
+      timeout)
   ]
 }
 handler.help = ['tebakgame']
