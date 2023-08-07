c311c3233a4ea4fc3e00f58796778db418ce7804
nando <Rifando.p.p@gmail.com>
2023-08-07 16:10:33 +0700

    update plugins/_role.js

--- plugins/_role.js
+++ plugins/_role.js
@@ -17,13 +17,13 @@ const roles = {
 
 module.exports = {
   before(m) {
-    let user = global.db.data.users[m.sender];
-    let level = user.level;
+    let user = global.db.data.users[m.sender]
+    let level = user.level
     let role = (Object.entries(roles)
       .sort((a, b) => b[1] - a[1])
       .find(([, minLevel]) => level >= minLevel) ||
-      Object.entries(roles)[0])[0];
-    user.role = role;
-    return true;
+      Object.entries(roles)[0])[0]
+    user.role = role
+    return true
   },
 };
