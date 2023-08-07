f4ee38ab2b8a5a9d0a792d95b7bf5d88ad93c4bb
nando <Rifando.p.p@gmail.com>
2023-08-07 12:35:06 +0700

    update plugins/game-family100.js

--- plugins/game-family100.js
+++ plugins/game-family100.js
@@ -1,31 +1,31 @@
 let fetch = require('node-fetch')
 let winScore = 1000
 async function handler(m) {
-    if (!db.data.settings[conn.user.jid].game) throw dfail('game', m, conn)
-    this.game = this.game ? this.game : {}
-    let id = 'family100_' + m.chat
-    if (id in this.game) {
-        this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
-        throw false
-    }
+  if (!db.data.settings[conn.user.jid].game) return m.reply(status.game)
+  this.game = this.game ? this.game: {}
+  let id = 'family100_' + m.chat
+  if (id in this.game) {
+    this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
+    throw false
+  }
   let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `
-*Soal:* ${json.soal}
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `
+  *Soal:* ${json.soal}
 
-Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
-(beberapa jawaban terdapat spasi)
-`: ''}
+  Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
+  (beberapa jawaban terdapat spasi)
+  `: ''}
 
-+${winScore} Money tiap jawaban benar
-    `.trim()
-    this.game[id] = {
-        id,
-        msg: await m.reply(caption),
-        ...json,
-        terjawab: Array.from(json.jawaban, () => false),
-        winScore,
-    }
+  +${winScore} Money tiap jawaban benar
+  `.trim()
+  this.game[id] = {
+    id,
+    msg: await m.reply(caption),
+    ...json,
+    terjawab: Array.from(json.jawaban, () => false),
+    winScore,
+  }
 }
 handler.help = ['family100']
 handler.tags = ['game']
