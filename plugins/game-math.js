2312b4a0e70fb3ffcc5c0d413155f15f53feaf80
nando <Rifando.p.p@gmail.com>
2023-08-07 12:40:30 +0700

    update plugins/game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -1,27 +1,31 @@
-
-let handler = async (m, { conn, args, usedPrefix }) => {
-  conn.math = conn.math ? conn.math : {}
+let handler = async (m, {
+  conn, args, usedPrefix
+}) => {
+  if (!db.data.settings[conn.user.jid].game) return m.reply(status.game)
+  conn.math = conn.math ? conn.math: {}
   if (args.length < 1) throw `
-Mode: ${Object.keys(modes).join(' | ')}
+  Mode: ${Object.keys(modes).join(' | ')}
 
-Contoh penggunaan: ${usedPrefix}math medium
-`.trim()
+  Contoh penggunaan: ${usedPrefix}math medium
+  `.trim()
   let mode = args[0].toLowerCase()
   if (!(mode in modes)) throw `
-Mode: ${Object.keys(modes).join(' | ')}
+  Mode: ${Object.keys(modes).join(' | ')}
 
-Contoh penggunaan: ${usedPrefix}math medium
-`.trim()
+  Contoh penggunaan: ${usedPrefix}math medium
+  `.trim()
   let id = m.chat
   if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
   let math = genMath(mode)
   conn.math[id] = [
     await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP, 1 Tiketcoin`, m),
-    math, 4,
+    math,
+    4,
     setTimeout(() => {
       if (conn.math[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, conn.math[id][0])
       delete conn.math[id]
-    }, math.time)
+    },
+      math.time)
   ]
 }
 handler.help = ['math']
@@ -51,11 +55,15 @@ let operators = {
 
 function genMath(mode) {
   let [a1, a2, b1, b2, ops, time, bonus] = modes[mode]
-  let a = randomInt(a1, a2)
-  let b = randomInt(b1, b2)
+  let a = randomInt(a1,
+    a2)
+  let b = randomInt(b1,
+    b2)
   let op = pickRandom([...ops])
-  let result = (new Function(`return ${a} ${op.replace('/', '*')} ${b < 0 ? `(${b})` : b}`))()
-  if (op == '/') [a, result] = [result, a]
+  let result = (new Function(`return ${a} ${op.replace('/', '*')} ${b < 0 ? `(${b})`: b}`))()
+  if (op == '/') [a,
+    result] = [result,
+    a]
   return {
     str: `${a} ${operators[op]} ${b}`,
     mode,
@@ -66,7 +74,9 @@ function genMath(mode) {
 }
 
 function randomInt(from, to) {
-  if (from > to) [from, to] = [to, from]
+  if (from > to) [from,
+    to] = [to,
+    from]
   from = Math.floor(from)
   to = Math.floor(to)
   return Math.floor((to - from) * Math.random() + from)
