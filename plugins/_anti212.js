e47fc9e2241828d77a26e9021b8b091e9c719315
nando <Rifando.p.p@gmail.com>
2023-08-07 16:03:03 +0700

    update plugins/_anti212.js

---
+++ plugins/_anti212.js
@@ -0,0 +1,12 @@
+let handler = m => m
+
+handler.before = async function(m) {
+  if (m.sender.startsWith('212' || '212')) {
+    global.db.data.users[m.sender].banned = true
+    await conn.updateBlockStatus(m.sender, "block")
+  }
+  if (m.sender.startsWith('265' || '265')) {
+    global.db.data.users[m.sender].banned = true
+  }
+}
+module.exports = handler
