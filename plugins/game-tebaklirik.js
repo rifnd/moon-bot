298fd97dae42e0343ebcc2f0606bae7bb359fd87
nando <Rifando.p.p@gmail.com>
2023-08-07 12:44:49 +0700

    update plugins/game-tebaklirik.js

--- plugins/game-tebaklirik.js
+++ plugins/game-tebaklirik.js
@@ -3,34 +3,38 @@ const fetch = require('node-fetch')
 
 let timeout = 180000
 let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
-    if (!db.data.settings[conn.user.jid].game) throw dfail('game', m, conn)
-    conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik : {}
-    let id = m.chat
-    if (id in conn.tebaklirik) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebaklirik[id][0])
-        throw false
-    }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebaklirik.json')
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
-    let caption = `
-${json.soal}
+let handler = async (m, {
+  conn, usedPrefix
+}) => {
+  if (!db.data.settings[conn.user.jid].game) return m.reply(status.game)
+  conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik: {}
+  let id = m.chat
+  if (id in conn.tebaklirik) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebaklirik[id][0])
+    throw false
+  }
+  let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebaklirik.json')
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
+  let data = await res.json()
+  let json = data[Math.floor(Math.random() * data.length)]
+  let caption = `
+  ${json.soal}
 
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}teli untuk bantuan
-Bonus: ${poin} XP
-TiketCoin: 1 Tiketcoin
-    `.trim()
-    conn.tebaklirik[id] = [
-        await conn.reply(m.chat, caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebaklirik[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebaklirik[id][0])
-            delete conn.tebaklirik[id]
-        }, timeout)
-    ]
+  Timeout *${(timeout / 1000).toFixed(2)} detik*
+  Ketik ${usedPrefix}teli untuk bantuan
+  Bonus: ${poin} XP
+  TiketCoin: 1 Tiketcoin
+  `.trim()
+  conn.tebaklirik[id] = [
+    await conn.reply(m.chat, caption, m),
+    json,
+    poin,
+    setTimeout(() => {
+      if (conn.tebaklirik[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebaklirik[id][0])
+      delete conn.tebaklirik[id]
+    },
+      timeout)
+  ]
 }
 handler.help = ['tebaklirik']
 handler.tags = ['game']
