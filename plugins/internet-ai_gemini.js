503f2877d06d3cd8bf42b7f3b01a8d14fb6ac3c7
rifando.p.p@gmail.com <rifando.p.p@gmail.com>
2024-01-24 20:19:00 +0700

    + gemini ai

--- plugins/internet-ai_gemini.js
+++ plugins/internet-ai_gemini.js
@@ -5,16 +5,31 @@ let handler = async (m, {
 }) => {
   try {
     if (!text) return m.reply(Func.example(usedPrefix, command, 'moon-bot'))
-    conn.react(m.chat, '🕒', m.key)
-    const json = await Func.fetchJson(API('alya', '/api/gemini', { q: text }, 'apikey'))
-    if (!json.status) return m.reply(Func.jsonFormat(json))
-    m.reply(json.data.content)
+    m.react('🕒')
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image\/(jpe?g|png)/.test(mime)) {
+      try {
+        let img = await q.download()
+        let res = await scrap.uploader(img)
+        let respon = await Func.fetchJson(API('alya', '/api/func-chat', { model: 'gemini', system: text, image: res.data.url }, 'apikey'))
+        if (!respon.status) return m.reply(Func.jsonFormat(respon))
+        m.reply(respon.data.content)
+      } catch (i) {
+        return conn.reply(m.chat, Func.jsonFormat(i), m)
+      }
+    } else if (text) {
+      const cok = await Func.fetchJson(API('alya', '/api/gemini', { q: text }, 'apikey'))
+      if (!cok.status) return m.reply(Func.jsonFormat(cok))
+      m.reply(cok.data.content)
+    }
   } catch (e) {
     console.log(e)
-    return m.reply(Func.jsonFormat(e))
+    m.reply(e.msg)
   }
 }
-handler.help = handler.command = ['ai-gemini']
+handler.help = ['gemini']
 handler.tags = ['internet']
+handler.command = /^(gemini)$/i
 handler.limit = true
 module.exports = handler
