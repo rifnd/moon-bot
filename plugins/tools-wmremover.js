c982f8cf47c474dc27ae597e46ab0100eaf1e3b2
Nando <Rifando.p.p@gmail.com>
2024-03-29 23:01:51 +0700

    add : text & wm remover

---
+++ plugins/tools-wmremover.js
@@ -0,0 +1,27 @@
+let handler = async (m, {
+  conn,
+  usedPrefix,
+  command,
+  text
+}) => {
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`Send or reply to images with commands : *"${usedPrefix + command}"*`)
+    let res = await scrap.uploader(await q.download())
+    m.react('🕒')
+    let json = await Func.fetchJson(API('alya', '/api/watermark-remover', {
+      image: res.data.url
+    }, 'apikey'))
+    if (!json.status) return m.reply(Func.jsonFormat(json))
+    conn.sendFile(m.chat, json.data.url, '', global.set.wm, m)
+  } catch (e) {
+    console.log(e)
+    return m.reply(Func.jsonFormat(e))
+  }
+}
+handler.help = ['wmremover']
+handler.tags = ['tools']
+handler.command = /^(wmremover)$/i
+handler.limit = true
+module.exports = handler
