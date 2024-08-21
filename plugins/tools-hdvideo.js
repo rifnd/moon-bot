a6e35732f0548c7e0843f9d291834d4781f43b0b
Nando <Rifando.p.p@gmail.com>
2024-08-21 13:23:59 +0000

    feat: add some plugins

---
+++ plugins/tools-hdvideo.js
@@ -0,0 +1,47 @@
+module.exports = {
+   run: async (m, {
+      conn,
+      usedPrefix,
+      Scraper,
+      Func
+   }) => {
+      try {
+         if (m.quoted ? m.quoted.message : m.msg.viewOnce) {
+            let type = m.quoted ? Object.keys(m.quoted.message)[0] : m.mtype
+            let q = m.quoted ? m.quoted.message[type] : m.msg
+            if (/video/.test(type)) {
+               m.react('🕒')
+               let old = new Date()
+               let vid = await conn.downloadMediaMessage(q)
+               let result = await Scraper.uploader(vid)
+               const json = await Api.get('api/remini-video', {
+                  video: result.data.url
+               })
+               if (!json.status) return m.reply(Func.jsonFormat(json))
+               conn.sendFile(m.chat, json.data.url, '', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
+            } else conn.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
+         } else {
+            let q = m.quoted ? m.quoted : m
+            let mime = (q.msg || q).mimetype || ''
+            if (!mime) return conn.reply(m.chat, Func.texted('bold', `🚩 Reply photo.`), m)
+            if (!/video\/(mp4)/.test(mime)) return conn.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
+            m.react('🕒')
+            let old = new Date()
+            let vid = await q.download()
+            let result = await Scraper.uploader(vid)
+            const json = await Api.get('api/remini-video', {
+               video: result.data.url
+            })
+            if (!json.status) return m.reply(Func.jsonFormat(json))
+            conn.sendFile(m.chat, json.data.url, '', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
+         }
+      } catch (e) {
+         return conn.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   help: ['hdvideo'],
+   use: 'reply video',
+   tags: ['tools'],
+   command: /^(hdvideo)$/i,
+   premium: true,
+}
---
+++ plugins/tools-shorten.js
@@ -0,0 +1,27 @@
+module.exports = {
+   run: async (m, {
+      conn,
+      usedPrefix,
+      command,
+      args,
+      Scraper,
+      Func
+   }) => {
+      try {
+         if (!args[0]) return conn.reply(m.chat, Func.example(usedPrefix, command, 'http://google.com'), m)
+         m.react('🕒')
+         let json = await Api.get('api/shorten', {
+            url: args[0]
+         })
+         if (!json.status) return conn.reply(m.chat, Func.jsonFormat(json), m)
+         conn.reply(m.chat, json.data.url, m)
+      } catch (e) {
+         return conn.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   help: ['shortner'],
+   use: 'link',
+   command: /^(shortner|short)$/i,
+   tags: 'tools',
+   limit: true
+}
