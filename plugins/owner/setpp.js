1393ec779198d687fb22cbbac48908906f292370
rifnd <rifando.p.p@gmail.com>
2024-10-23 17:25:45 +0700

    fix: setpp

--- plugins/owner/setpp.js
+++ plugins/owner/setpp.js
@@ -1,3 +1,4 @@
+const { S_WHATSAPP_NET } = require('@whiskeysockets/baileys')
 module.exports = {
    help: ['setpp'],
    use: 'reply photo',
@@ -15,8 +16,25 @@ module.exports = {
          if (/image\/(jpe?g|png)/.test(mime)) {
             m.react('🕒')
             const buffer = await q.download()
-            await conn.updateProfilePicture(conn.user.jid, buffer)
-            await Func.delay(3000).then(() => conn.reply(m.chat, Func.texted('bold', `🚩 Profile photo has been successfully changed.`), m))
+            const { img } = await Func.generateProfilePicture(buffer)
+            await conn.query({
+               tag: 'iq',
+               attrs: {
+                  to: S_WHATSAPP_NET,
+                  type: 'set',
+                  xmlns: 'w:profile:picture'
+               },
+               content: [
+                  {
+                     tag: 'picture',
+                     attrs: {
+                        type: 'image'
+                     },
+                     content: img
+                  }
+               ]
+            })
+            conn.reply(m.chat, Func.texted('bold', `🚩 Profile photo has been successfully changed.`), m)
          } else return conn.reply(m.chat, Func.texted('bold', `🚩 Reply to the photo that will be made into the bot's profile photo.`), m)
       } catch (e) {
          conn.reply(m.chat, Func.jsonFormat(e), m)
