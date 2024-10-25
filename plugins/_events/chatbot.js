cb1230140226a17ba4c53665ef0c7e1e8e851563
rifnd <Rifando.p.p@gmail.com>
2024-10-25 15:19:54 +0000

    update

--- plugins/_events/chatbot.js
+++ plugins/_events/chatbot.js
@@ -8,12 +8,12 @@ module.exports = {
    }) {
       try {
          if (setting.chatbot && body && !env.evaluate_chars.some(v => body.startsWith(v)) && !m.isGroup) {
-            const json = await Api.get('api/ai-logic', {
-               system: 'Sekarang nama kamu adalah moon',
-               prompt: body,
+            const json = await Api.post('api/completions', {
+               model: 'meta-llama/Meta-Llama-3.1-70B-Instruct',
+               messages: JSON.stringify([{ role: 'system', content: 'Be a helpful assistant' }, { role: 'user', content: `${body}` }])
             })
             if (!json.status) return console.log(json)
-            if (!m.fromMe && !m.isGroup && json.status) return conn.reply(m.chat, json.data.content, null)
+            if (!m.fromMe && !m.isGroup && json.status) return conn.reply(m.chat, json.data.choices[0].message.content, null)
          }
       } catch (e) {
          console.log(e)
